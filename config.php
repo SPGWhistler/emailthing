188e96d4a8c8ae610910f2c29c12910b46a0c2f1
Tony Petty <tpetty@appmobi.com>
2013-10-24 13:11:28 -0400

    Got update working.

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'appsignature' => 'none',
+	'clientid' => 'qe9a3m3z8y5yy4ynxgr7fs3e',
+	'clientsecret' => 'WPaFqvEEZACPpFRDjnGdje2W',
+	'defaultwsdl' => 'https://webservice.exacttarget.com/etframework.wsdl'
+);
+
+?>
